3f1902bcf641ad1d73b860233b23a356a8b0cce0
Marco Elver <elver@google.com>
2025-02-10 18:57:37 +0100

    compiler-context-analysis: Remove __cond_lock() function-like helper
    As discussed in [1], removing __cond_lock() will improve the readability
    of trylock code. Now that Sparse context tracking support has been
    removed, we can also remove __cond_lock().
    
    Change existing APIs to either drop __cond_lock() completely, or make
    use of the __cond_acquires() function attribute instead.
    
    In particular, spinlock and rwlock implementations required switching
    over to inline helpers rather than statement-expressions for their
    trylock_* variants.
    
    Link: https://lore.kernel.org/all/20250207082832.GU7145@noisy.programming.kicks-ass.net/ [1]
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Marco Elver <elver@google.com>
    ---
    v2:
    * New patch.

--- include/linux/compiler-context-analysis.h
+++ include/linux/compiler-context-analysis.h
@@ -330,24 +330,6 @@ static inline void _context_unsafe_alias(void **p) { }
 #define __release(x)		__release_ctx_guard(x)
 
 /**
- * __cond_lock() - function that conditionally acquires a context guard
- *                 exclusively
- * @x: context guard instance pinter
- * @c: boolean expression
- *
- * Return: result of @c
- *
- * No-op function that conditionally acquires context guard instance @x
- * exclusively, if the boolean expression @c is true. The result of @c is the
- * return value; for example:
- *
- * .. code-block:: c
- *
- *	#define spin_trylock(l) __cond_lock(&lock, _spin_trylock(&lock))
- */
-#define __cond_lock(x, c)	__try_acquire_ctx_guard(x, c)
-
-/**
  * __must_hold_shared() - function attribute, caller must hold shared context guard
  *
  * Function attribute declaring that the caller must hold the given context
@@ -404,19 +386,6 @@ static inline void _context_unsafe_alias(void **p) { }
  * access.
  */
 #define __release_shared(x)	__release_shared_ctx_guard(x)
-
-/**
- * __cond_lock_shared() - function that conditionally acquires a context guard shared
- * @x: context guard instance pinter
- * @c: boolean expression
- *
- * Return: result of @c
- *
- * No-op function that conditionally acquires context guard instance @x with
- * shared access, if the boolean expression @c is true. The result of @c is the
- * return value.
- */
-#define __cond_lock_shared(x, c) __try_acquire_shared_ctx_guard(x, c)
 
 /**
  * __acquire_ret() - helper to acquire context guard of return value
