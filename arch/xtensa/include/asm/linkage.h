0b8a0a0dea02d92411ea19d614d7dd03d67fb245
Marco Elver <elver@google.com>
2026-05-08 14:51:35 +0200

    xtensa: Implement _THIS_IP_ using inline asm
    Both GCC [1] and Clang [2] consider the generic version of _THIS_IP_ to
    be broken:
    
            #define _THIS_IP_  ({ __label__ __here; __here: (unsigned long)&&__here; })
    
    In particular, the address of a label is only expected to be used with a
    computed goto.
    
    While the generic version more or less works today, it is known to be
    brittle and may break with current and future optimizations. For
    example, Clang -O2 always returns 1 when this function is inlined:
    
            static inline unsigned long get_ip(void)
            { return ({ __label__ __here; __here: (unsigned long)&&__here; }); }
    
    Fix it by overriding _THIS_IP_ in <asm/linkage.h> (which is included by
    <linux/instruction_pointer.h>) using an architecture-specific inline asm
    version. Additionally, avoiding taking the address of a label prevents
    compilers from emitting spurious indirect branch targets (e.g. ENDBR or
    BTI) under control-flow integrity schemes.
    
    Link: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=120071 [1]
    Link: https://github.com/llvm/llvm-project/issues/138272 [2]
    Signed-off-by: Marco Elver <elver@google.com>

--- arch/xtensa/include/asm/linkage.h
+++ arch/xtensa/include/asm/linkage.h
@@ -6,4 +6,15 @@
 #define __ALIGN		.align 4
 #define __ALIGN_STR	".align 4"
 
+#define _THIS_IP_ ({				\
+	unsigned long __ip;			\
+	asm volatile(".align 4\n\t"		\
+		     "call0 1f\n\t"		\
+		     ".align 4\n\t"		\
+		     "1: mov %0, a0"		\
+		     : "=r" (__ip) : : "a0"	\
+	);					\
+	__ip;					\
+})
+
 #endif
